ceb94fe67271ddd904d8c1b9cb5ebef926806634
Udit <udit@eskaytech.com>
2023-01-03 02:39:38 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function main() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    let id_token = await core.getIDToken()
+    console.log('id_token', id_token)
+    core.setOutput('id_token', id_token)
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  } 
+}
+
+main()
